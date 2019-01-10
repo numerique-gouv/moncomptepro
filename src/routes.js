bd1b50b0589b97743011fa509ec646d6e3e787ab
Raphaël Dubigny <rdubigny@gmail.com>
2019-01-10 11:57:51 +0100

    hotfix: user not in session error for session longer than one day

--- src/routes.js
+++ src/routes.js
@@ -108,6 +108,8 @@ module.exports = (app, provider) => {
   app.get('/interaction/:grant/login', async (req, res, next) => {
     try {
       if (isEmpty(req.session.user)) {
+        // This may occur if the oidc session cookie duration (maxAge) is longer
+        // in time than the express session one.
         return next(new Error('user not in session'));
       }
 
@@ -117,6 +119,11 @@ module.exports = (app, provider) => {
           acr: 'urn:mace:incommon:iap:bronze',
           amr: ['pwd'],
           ts: Math.floor(Date.now() / 1000),
+          // Set the session cookie as persistent (vs transient) to limit the session duration to
+          // the duration specified in oidc provider long cookie maxAge configuration.
+          // Transient cookies can result in non expiring session as mentioned in
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Cookies#Session_cookies
+          remember: true,
         },
         // We assume all oidc_clients can access the user data without asking for his consent.
         // So, we do not prompt the user for his consent, and we hard code the consent to all result here.
