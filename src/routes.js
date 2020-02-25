a3ff0f2735aeba93a0917196544557d488985d62
Raphaël Dubigny <rdubigny@gmail.com>
2020-02-25 16:58:19 +0100

    update link to api.gouv.fr

--- src/routes.js
+++ src/routes.js
@@ -190,7 +190,7 @@ module.exports = (app, provider) => {
       if (error instanceof SessionNotFound) {
         // we may have took to long to provide a session to the user since he has been redirected
         // we fail silently
-        return res.redirect('https://api.gouv.fr/?filter=signup');
+        return res.redirect('https://api.gouv.fr/rechercher-api?filter=signup');
       }
 
       next(error);
@@ -250,7 +250,7 @@ module.exports = (app, provider) => {
           return res.redirect(req.body.referer);
         }
 
-        return res.redirect('https://api.gouv.fr/?filter=signup');
+        return res.redirect('https://api.gouv.fr/rechercher-api?filter=signup');
       } catch (error) {
         if (error.message === 'invalid_credentials') {
           return res.redirect(`/users/sign-in?notification=${error.message}`);
@@ -358,7 +358,7 @@ module.exports = (app, provider) => {
           );
         }
 
-        return res.redirect('https://api.gouv.fr/?filter=signup');
+        return res.redirect('https://api.gouv.fr/rechercher-api?filter=signup');
       } catch (error) {
         if (error.message === 'unable_to_auto_join_organization') {
           return res.redirect(
