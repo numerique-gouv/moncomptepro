45718a4a4ba74c72f804b7258befd760e1fcf10b
Stanislas Bernard <bernardstanislas@gmail.com>
2021-06-15 16:01:44 +0200

    :sparkles: Capture if user is external

--- src/repositories/moderation.js
+++ src/repositories/moderation.js
@@ -1,11 +1,15 @@
 import _ from 'lodash';
 import { getDatabaseConnection } from '../connectors/postgres';
 
-export const createOrganizationJoinBlock = async (user, organization) => {
+export const createOrganizationJoinBlock = async (
+  user,
+  organization,
+  asExternal
+) => {
   const connection = getDatabaseConnection();
 
   await connection.query(
-    `INSERT INTO moderations (user_id, organization_id, type) VALUES ($1, $2, $3);`,
-    [user.id, organization.id, 'organization_join_block']
+    `INSERT INTO moderations (user_id, organization_id, type, as_external) VALUES ($1, $2, $3, $4);`,
+    [user.id, organization.id, 'organization_join_block', asExternal]
   );
 };
