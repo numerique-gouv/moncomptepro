f99d8ec48c52d5e2141364b068006ac7e25df3e1
Stanislas Bernard <bernardstanislas@gmail.com>
2021-06-15 14:47:48 +0200

    :card_file_box: Add moderation table

---
+++ migrations/1623752868625_add-moderation-table.js
@@ -0,0 +1,25 @@
+exports.shorthands = undefined;
+
+exports.up = async pgm => {
+  await pgm.db.query(`
+CREATE TYPE moderation_type AS ENUM('organization_join_block');
+CREATE TABLE moderations (
+    id serial,
+    user_id int NOT NULL,
+    organization_id int NOT NULL,
+    type moderation_type,
+    created_at timestamp NOT NULL DEFAULT NOW(),
+    moderated_at timestamp,
+    PRIMARY KEY(id),
+    FOREIGN KEY(user_id)
+	REFERENCES users(id)
+	ON DELETE CASCADE,
+    FOREIGN KEY(organization_id)
+	REFERENCES organizations(id)
+	ON DELETE CASCADE
+);`);
+};
+
+exports.down = async pgm => {
+  await pgm.db.query(`DROP TABLE moderations; DROP TYPE moderation_type;`);
+};
