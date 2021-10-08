162302f54fe785a451398d76c30940eb7ef9c6ac
Raphaël Dubigny <rdubigny@gmail.com>
2021-10-08 16:42:34 +0200

    feat: less mails send to admin + use moderation table for big organisation join notification

--- src/repositories/moderation.js
+++ src/repositories/moderation.js
@@ -1,15 +1,41 @@
-import _ from 'lodash';
 import { getDatabaseConnection } from '../connectors/postgres';
 
-export const createOrganizationJoinBlock = async (
-  user,
-  organization,
-  asExternal
-) => {
+const createModeration = async ({
+  user_id,
+  organization_id,
+  type,
+  as_external,
+}) => {
   const connection = getDatabaseConnection();
 
   await connection.query(
     `INSERT INTO moderations (user_id, organization_id, type, as_external) VALUES ($1, $2, $3, $4);`,
-    [user.id, organization.id, 'organization_join_block', asExternal]
+    [user_id, organization_id, type, as_external]
   );
+};
+
+export const createOrganizationJoinBlockModeration = async ({
+  user_id,
+  organization_id,
+  as_external,
+}) => {
+  await createModeration({
+    user_id,
+    organization_id,
+    type: 'organization_join_block',
+    as_external,
+  });
+};
+
+export const createBigOrganizationJoinModeration = async ({
+  user_id,
+  organization_id,
+  as_external,
+}) => {
+  await createModeration({
+    user_id,
+    organization_id,
+    type: 'big_organization_join',
+    as_external,
+  });
 };
