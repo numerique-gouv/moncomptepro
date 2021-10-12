df0df11498a35015332081993cf2473dbf652c4f
Raphaël Dubigny <rdubigny@gmail.com>
2021-10-12 13:32:49 +0200

    feat: update matomo configuration

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.use(
     directives: {
       defaultSrc: ["'self'"],
       imgSrc: ["'self'", 'stats.data.gouv.fr'],
-      connectSrc: ["'self'", 'entreprise.data.gouv.fr'],
+      connectSrc: ["'self'", 'entreprise.data.gouv.fr', 'stats.data.gouv.fr'],
       scriptSrc: ["'self'", 'stats.data.gouv.fr'],
       styleSrc: ["'self'", 'unpkg.com'],
       fontSrc: ["'self'", 'unpkg.com'],
