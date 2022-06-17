839c145bddbf4e6d1e203fce571a46fa7faaeccd
Raphaël Dubigny <rdubigny@gmail.com>
2022-06-17 19:12:13 +0200

    feat: design for sign-up page

--- src/notificationMessages.js
+++ src/notificationMessages.js
@@ -1,34 +1,33 @@
 const notificationMessages = {
   invalid_credentials: {
     type: 'error',
-    message: 'Mot de passe incorrect.',
+    description: 'Mot de passe incorrect.',
   },
   invalid_email: {
     type: 'error',
-    title: 'Erreur',
     description: 'Adresse email invalide.',
   },
   invalid_siret: {
     type: 'error',
-    message: 'SIRET invalide.',
+    description: 'SIRET invalide.',
   },
   invalid_token: {
     type: 'warning',
-    message:
+    description:
       'Le lien que vous avez utilisé est invalide ou expiré. Veuillez cliquer sur « Réinitialiser mon mot de passe » pour recevoir un nouveau lien',
   },
   password_change_success: {
     type: 'success',
-    message:
+    description:
       'Votre mot de passe a été mis à jour. Veuillez vous connecter avec votre nouveau mot de passe.',
   },
   reset_password_email_sent: {
     type: 'info',
-    message: 'Vous allez recevoir un lien de réinitialisation par e-mail.',
+    description: 'Vous allez recevoir un lien de réinitialisation par e-mail.',
   },
   unable_to_auto_join_organization: {
     type: 'info',
-    message: `Vous souhaitez rejoindre une organisation existante.
+    description: `Vous souhaitez rejoindre une organisation existante.
 
 Nous avons besoin de vérifier par nous-même que vous faites bien partie de cette organisation.
 Vous recevrez un email dès que nous aurons terminé (délai moyen : 5 jours ouvrés).
@@ -37,16 +36,16 @@ Si vous avez la moindre question, écrivez-nous à contact@api.gouv.fr`,
   },
   user_in_organization_already: {
     type: 'error',
-    message: 'Vous appartenez déjà à cette organisation.',
+    description: 'Vous appartenez déjà à cette organisation.',
   },
   email_unavailable: {
     type: 'warning',
-    message: `Un compte existe déjà avec cet email.
+    description: `Un compte existe déjà avec cet email.
       Si vous avez oublié votre mot de passe cliquez sur « Mot de passe oublié ? ».`,
   },
   email_verification_sent: {
     type: 'success',
-    message: `Un nouveau code de confirmation vous a été envoyé.
+    description: `Un nouveau code de confirmation vous a été envoyé.
 
 La réception de ce code peut prendre jusqu’à 15 minutes.
 Si vous n’avez rien reçu après ce délai, merci de vérifier votre boîte de SPAM.
@@ -56,21 +55,21 @@ Enfin, si vous avez essayé ces solutions et que vous ne recevez toujours pas le
   },
   email_verified_already: {
     type: 'error',
-    message: `Votre compte a déjà été confirmé.`,
+    description: `Votre compte a déjà été confirmé.`,
   },
   invalid_verify_email_code: {
     type: 'error',
-    message:
+    description:
       'Le code de confirmation que vous avez utilisé est invalide ou expiré.',
   },
   weak_password: {
     type: 'error',
-    message:
+    description:
       'Votre mot de passe est trop court. Merci de choisir un mot de passe d’au moins 10 caractères.',
   },
   invalid_personal_informations: {
     type: 'error',
-    message: 'Le format des informations personnelles est invalide.',
+    description: 'Le format des informations personnelles est invalide.',
   },
 };
 
