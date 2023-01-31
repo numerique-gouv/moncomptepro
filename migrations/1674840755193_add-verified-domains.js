c6729778e2a31e7bab22ec9756d15da6b655a0df
Raphaël Dubigny <rdubigny@gmail.com>
2023-01-31 09:49:30 +0100

    chore: enable verification storage

---
+++ migrations/1674840755193_add-verified-domains.js
@@ -0,0 +1,25 @@
+exports.shorthands = undefined;
+
+exports.up = async pgm => {
+  await pgm.db.query(`
+ALTER TABLE organizations
+ADD COLUMN verified_email_domains character varying[] DEFAULT '{}'::character varying[];
+`);
+
+  await pgm.db.query(`
+ALTER TABLE users_organizations
+ADD COLUMN verification_type character varying;
+`);
+};
+
+exports.down = async pgm => {
+  await pgm.db.query(`
+ALTER TABLE organizations
+DROP COLUMN verified_email_domains;
+`);
+
+  await pgm.db.query(`
+ALTER TABLE users_organizations
+DROP COLUMN verification_type;
+`);
+};
