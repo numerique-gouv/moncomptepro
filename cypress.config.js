171d7af68cd30dd3321a40b3cbad9634d166557c
Raphaël Dubigny <rdubigny@gmail.com>
2023-01-31 13:01:50 +0100

    chore: upgrade cypress

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 30000,
+  pageLoadTimeout: 30000,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config);
+    },
+  },
+};
