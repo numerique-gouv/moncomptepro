8ff1be422b7fb06a0036a640a2e83d911ad6f6b5
Raphaël Dubigny <rdubigny@gmail.com>
2023-05-15 18:25:15 +0200

    chore: use scalingo default env var

--- src/connectors/redis.ts
+++ src/connectors/redis.ts
@@ -1,9 +1,13 @@
 import RedisClient from 'ioredis';
 
+const {
+  REDIS_URL: connectionString = 'redis://:@127.0.0.1:6379',
+} = process.env;
+
 export const getNewRedisClient = (params = {}) => {
-  const redisClient = new RedisClient(params);
+  const redisClient = new RedisClient(connectionString, params);
   redisClient.on('connect', () =>
-    console.log('Connected to database : redis://:@127.0.0.1:6380')
+    console.log(`Connected to database : ${connectionString}`)
   );
 
   return redisClient;
