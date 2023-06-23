eeafdb92b06c21fbb7f8bd016ded7df685512913
Raphaël Dubigny <rdubigny@gmail.com>
2023-06-23 07:27:45 +0200

    chore: obfuscate postgresql password in logs

--- src/connectors/postgres.ts
+++ src/connectors/postgres.ts
@@ -3,6 +3,10 @@ import { Pool } from 'pg';
 let pool: Pool | null = null;
 
 const { DATABASE_URL: connectionString } = process.env;
+const obfuscatedConnectionString = (connectionString || '').replace(
+  /\/\/(.*)@/,
+  '//****:****@'
+);
 
 export const getDatabaseConnection = () => {
   if (pool) {
@@ -12,11 +16,11 @@ export const getDatabaseConnection = () => {
   pool = new Pool({ connectionString });
 
   pool.on('connect', client => {
-    console.log(`Connected to database : ${connectionString}`);
+    console.log(`Connected to database : ${obfuscatedConnectionString}`);
   });
 
   pool.on('remove', client => {
-    console.log(`Disconnected from database : ${connectionString}`);
+    console.log(`Disconnected from database : ${obfuscatedConnectionString}`);
   });
 
   pool.on('error', (error, client) => {
