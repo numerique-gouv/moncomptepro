95ac1cf58f3d75a0121ef48cc4cac6f68908b942
Raphaël Dubigny <rdubigny@gmail.com>
2023-09-06 16:22:27 +0200

    tests: add e2e test for connection from legacy and agentconnect client

--- cypress/e2e/signin_from_standard_client.cy.js
+++ cypress/e2e/signin_from_standard_client.cy.js
@@ -21,5 +21,28 @@ describe('sign-in from standard client', () => {
     cy.get('.fr-grid-row .fr-col-12:first-child .fr-tile__link').click();
 
     cy.contains('unused1@yopmail.com');
+    cy.contains('Commune de lamalou-les-bains - Mairie');
+  });
+
+  it('should sign-in with org selection', function () {
+    cy.visit(`http://localhost:4000`);
+    cy.get('button.moncomptepro-button').click();
+
+    cy.get('[name="login"]').type('unused2@yopmail.com');
+    cy.get('[type="submit"]').click();
+
+    cy.get('[name="password"]').type('password123');
+    cy.get('[action="/users/sign-in"]  [type="submit"]')
+      .contains('Se connecter')
+      .click();
+
+    cy.get('.fr-grid-row .fr-col-12:last-child .fr-tile__link').contains(
+      'Commune de clamart - Mairie'
+    );
+
+    cy.get('.fr-grid-row .fr-col-12:last-child .fr-tile__link').click();
+
+    cy.contains('unused2@yopmail.com');
+    cy.contains('Commune de clamart - Mairie');
   });
 });
