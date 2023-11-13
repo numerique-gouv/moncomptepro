0340e4ef7ddabe367f8bccaa696816b75d584363
Douglas Duteil <douglasduteil@gmail.com>
2023-11-13 12:21:31 +0100

    feat(types): add big_organization_join moderation type

--- src/types/moderation.d.ts
+++ src/types/moderation.d.ts
@@ -3,6 +3,7 @@ interface Moderation {
   user_id: number;
   organization_id: number;
   type:
+    | 'big_organization_join'
     | 'organization_join_block'
     | 'non_verified_domain'
     | 'ask_for_sponsorship';
