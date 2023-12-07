b84f0e1035cad381a0035122fdaf81014a1b500e
Raphaël Dubigny <rdubigny@gmail.com>
2023-12-07 15:12:23 +0100

    refactor: epoch time takes js date as optional params

---
+++ test/epoch-time.test.ts
@@ -0,0 +1,25 @@
+import { assert } from 'chai';
+import FakeTimers, { InstalledClock } from '@sinonjs/fake-timers';
+import epochTime from '../src/services/epoch-time';
+
+describe('isExpired', () => {
+  let clock: InstalledClock;
+
+  beforeEach(() => {
+    clock = FakeTimers.install({ now: new Date(2023, 11, 1) });
+  });
+
+  afterEach(() => {
+    clock.uninstall();
+  });
+
+  it('should return current date in epoch format', () => {
+    assert.strictEqual(epochTime(), 1701388800);
+  });
+
+  it('should return a date in epoch format', () => {
+    const emittedDate = new Date(2023, 11, 1, 0, 1);
+
+    assert.strictEqual(epochTime(emittedDate), 1701388860);
+  });
+});
