ebaf3da4907244498e3387a0542fb2e26243a223
Raphaël Dubigny <rdubigny@gmail.com>
2024-02-09 15:56:54 +0100

    tests: test is now independent of server timezone

--- test/epoch-time.test.ts
+++ test/epoch-time.test.ts
@@ -2,11 +2,11 @@ import { assert } from "chai";
 import FakeTimers, { InstalledClock } from "@sinonjs/fake-timers";
 import epochTime from "../src/services/epoch-time";
 
-describe("isExpired", () => {
+describe("epochTime", () => {
   let clock: InstalledClock;
 
   beforeEach(() => {
-    clock = FakeTimers.install({ now: new Date(2023, 11, 1) });
+    clock = FakeTimers.install({ now: new Date("2023-12-01T00:00:00.000Z") });
   });
 
   afterEach(() => {
@@ -18,7 +18,7 @@ describe("isExpired", () => {
   });
 
   it("should return a date in epoch format", () => {
-    const emittedDate = new Date(2023, 11, 1, 0, 1);
+    const emittedDate = new Date("2023-12-01T00:01:00.000Z");
 
     assert.strictEqual(epochTime(emittedDate), 1701388860);
   });
