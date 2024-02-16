db9c2a4f5d71b17b199bc1e5a9c2488d0f441def
Emmanuel Pelletier <manu@habite.la>
2024-02-16 14:26:40 +0100

    a11y: correctly vocalize password confirmation error to screen readers
    followup to previous commit, here we handle this specific error handling
    via the JS confirm password script.
    
    RGAA criteria 11.10

--- public/confirm-password_20230828.js
+++ public/confirm-password_20230828.js
@@ -1,12 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
   var password = document.getElementById("password-input");
   var confirm_password = document.getElementById("confirm_password");
+  var confirm_password_sr_error = document.getElementById("password-confirmation-error");
 
   function validatePassword() {
-    if (password.value !== confirm_password.value) {
-      confirm_password.setCustomValidity("Les mots de passe ne sont pas identiques");
+    if (confirm_password.value && password.value !== confirm_password.value) {
+      const errorMessage = "Les mots de passe ne sont pas identiques";
+      confirm_password.setCustomValidity(errorMessage);
+      confirm_password_sr_error.textContent = errorMessage;
     } else {
       confirm_password.setCustomValidity("");
+      confirm_password_sr_error.textContent = "";
     }
   }
 
