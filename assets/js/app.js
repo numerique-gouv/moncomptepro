d0bdfb1a8e766124895c253b405c021bf44eb7c2
rebeccadumazert <rebeccadumazert@hotmail.fr>
2024-05-15 14:55:50 +0200

    add dsfr header

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,7 @@
+import "@gouvfr/dsfr/dist/core/core.module";
+import "@gouvfr/dsfr/dist/component/navigation/navigation.module";
+import "@gouvfr/dsfr/dist/component/modal/modal.module";
+import "@gouvfr/dsfr/dist/component/header/header.module";
 import { explainExternalLinks } from "./modules/sr-external-links";
 import { disableFormsOnSubmit } from "./modules/disable-on-submit";
 import './modules/load-bar';
