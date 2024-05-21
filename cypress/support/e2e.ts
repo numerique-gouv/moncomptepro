422385662e8dd09d086b24a155ab77552e484d15
Raphaël Dubigny <rdubigny@gmail.com>
2024-05-21 15:21:46 +0200

    chore: add option to record tests with cypress to generate demo videos

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -17,3 +17,29 @@ import "cypress-axe";
 import "cypress-maildev";
 
 import "./commands";
+
+const RECORD = Cypress.env("RECORD") === true;
+
+if (RECORD) {
+  ["visit", "click", "trigger", "type", "clear", "reload", "select"].forEach(
+    (command) => {
+      Cypress.Commands.overwrite(
+        command as unknown as keyof Cypress.Chainable<any>,
+        (originalFn, ...args) => {
+          const origVal = originalFn(...args);
+
+          return new Promise((resolve) => {
+            setTimeout(
+              () => {
+                resolve(origVal);
+              },
+              RECORD ? 2000 : 0,
+            );
+          });
+        },
+      );
+    },
+  );
+  Cypress.config("viewportWidth", 2560);
+  Cypress.config("viewportHeight", 1440);
+}
