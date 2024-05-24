600ff3023ff1b940acd362bd3f08e1b707e19e98
Raphaël Dubigny <rdubigny@gmail.com>
2024-05-24 17:41:57 +0200

    refactor: lint file

--- assets/js/webauthn-authentication.js
+++ assets/js/webauthn-authentication.js
@@ -1,43 +1,53 @@
-import { startAuthentication } from '@simplewebauthn/browser';
+import { startAuthentication } from "@simplewebauthn/browser";
 
-document.addEventListener('DOMContentLoaded', function() {
-  const beginElement = document.getElementById('webauthn-btn-begin-authentication');
-  const authenticationResponseStringInputElement = document.querySelector('input[name="webauthn_authentication_response_string"]');
-  const authenticationResponseForm = document.getElementById('webauthn-authentication-response-form');
-  const errorElement = document.getElementById('webauthn-alert-error');
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    const beginElement = document.getElementById(
+      "webauthn-btn-begin-authentication",
+    );
+    const authenticationResponseStringInputElement = document.querySelector(
+      'input[name="webauthn_authentication_response_string"]',
+    );
+    const authenticationResponseForm = document.getElementById(
+      "webauthn-authentication-response-form",
+    );
+    const errorElement = document.getElementById("webauthn-alert-error");
 
-// Start registration when the user clicks a button
-  const onAuthenticateClick = async () => {
-    // Reset success/error messages
-    errorElement.style.display = 'none';
-    errorElement.innerText = '';
-    beginElement.disabled = true;
+    // Start registration when the user clicks a button
+    const onAuthenticateClick = async () => {
+      // Reset success/error messages
+      errorElement.style.display = "none";
+      errorElement.innerText = "";
+      beginElement.disabled = true;
 
-    // GET registration options from the endpoint that calls
-    // @simplewebauthn/server -> generateRegistrationOptions()
-    const resp = await fetch('/api/webauthn/generate-authentication-options');
+      // GET registration options from the endpoint that calls
+      // @simplewebauthn/server -> generateRegistrationOptions()
+      const resp = await fetch("/api/webauthn/generate-authentication-options");
 
-    let asseResp;
-    try {
-      // Pass the options to the authenticator and wait for a response
-      asseResp = await startAuthentication(await resp.json());
-    } catch (error) {
-      errorElement.style.display = 'block';
-      if (error.name === 'NotAllowedError') {
-        errorElement.innerText = `Une erreur est survenue. Nous n’avons pas pu vérifier vos informations. Merci de réessayer.`;
-      } else {
-        errorElement.innerText = `Une erreur est survenue. Erreur: ${JSON.stringify(error, null, 2)}`;
+      let asseResp;
+      try {
+        // Pass the options to the authenticator and wait for a response
+        asseResp = await startAuthentication(await resp.json());
+      } catch (error) {
+        errorElement.style.display = "block";
+        if (error.name === "NotAllowedError") {
+          errorElement.innerText = `Une erreur est survenue. Nous n’avons pas pu vérifier vos informations. Merci de réessayer.`;
+        } else {
+          errorElement.innerText = `Une erreur est survenue. Erreur: ${JSON.stringify(error, null, 2)}`;
+        }
+
+        beginElement.disabled = false;
+        throw error;
       }
 
-      beginElement.disabled = false;
-      throw error;
-    }
+      // POST the response to the endpoint that calls
+      // @simplewebauthn/server -> verifyRegistrationResponse()
+      authenticationResponseStringInputElement.value = JSON.stringify(asseResp);
+      authenticationResponseForm.requestSubmit();
+    };
 
-    // POST the response to the endpoint that calls
-    // @simplewebauthn/server -> verifyRegistrationResponse()
-    authenticationResponseStringInputElement.value = JSON.stringify(asseResp);
-    authenticationResponseForm.requestSubmit();
-  };
-
-  beginElement.addEventListener('click', onAuthenticateClick);
-}, false);
+    beginElement.addEventListener("click", onAuthenticateClick);
+  },
+  false,
+);
