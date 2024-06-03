18ab92bf7ed97da244699769a6cb2e474afff365
Loïc Delmaire <loic.delmaire@gmail.com>
2024-06-03 17:40:51 +0200

    Fixtures: remove irrelevant URIs for DataPass

--- scripts/fixtures.sql
+++ scripts/fixtures.sql
@@ -263,14 +263,8 @@ VALUES
      'https://sandbox1.v2.datapass.api.gouv.fr/auth/mon_compte_pro/callback',
      'https://sandbox2.v2.datapass.api.gouv.fr/auth/mon_compte_pro/callback',
      'https://sandbox.api-entreprise.v2.datapass.api.gouv.fr/auth/mon_compte_pro/callback',
-     'https://sandbox1.api-entreprise.v2.datapass.api.gouv.fr/auth/mon_compte_pro/callback',
-     'https://sandbox2.api-entreprise..v2.datapass.api.gouv.fr/auth/mon_compte_pro/callback',
      'https://staging.v2.datapass.api.gouv.fr/auth/mon_compte_pro/callback',
-     'https://staging1.v2.datapass.api.gouv.fr/auth/mon_compte_pro/callback',
-     'https://staging2.v2.datapass.api.gouv.fr/auth/mon_compte_pro/callback',
-     'https://staging.api-entreprise.v2.datapass.api.gouv.fr/auth/mon_compte_pro/callback',
-     'https://staging1.api-entreprise.v2.datapass.api.gouv.fr/auth/mon_compte_pro/callback',
-     'https://staging2.api-entreprise..v2.datapass.api.gouv.fr/auth/mon_compte_pro/callback'
+     'https://staging.api-entreprise.v2.datapass.api.gouv.fr/auth/mon_compte_pro/callback'
      ],
    ARRAY [
      'http://localhost:3000',
