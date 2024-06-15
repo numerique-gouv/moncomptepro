6ad91c52d56e459f462ec35f285609249624f14d
Raphaël Dubigny <rdubigny@gmail.com>
2024-06-15 10:01:10 +0200

    tests: add e2e test signin with totp

---
+++ cypress/e2e/signin_with_totp.cy.js
@@ -0,0 +1,48 @@
+import { generateToken } from "@sunknudsen/totp";
+
+describe("sign-in with TOTP on untrusted browser", () => {
+  it("should sign-in with password and TOTP", function () {
+    cy.visit(`http://localhost:4001`);
+    cy.get("button.moncomptepro-button").click();
+    cy.get('[name="password"]').type("password123");
+    cy.get('[action="/users/sign-in"]  [type="submit"]')
+      .contains("S’identifier")
+      .click();
+
+    cy.contains("Valider en deux étapes");
+
+    const totp = generateToken("din5ncvbluqpx7xfzqcybmibmtjocnsf", Date.now());
+    cy.get("[name=totpToken]").type(totp);
+    cy.get(
+      '[action="/users/mfa-sign-in-with-authenticator"] [type="submit"]',
+    ).click();
+
+    cy.contains('"amr": [\n    "pwd",\n    "totp",\n    "mfa"\n  ],');
+  });
+
+  it("should trigger totp rate limiting", function () {
+    cy.visit(`/users/start-sign-in`);
+
+    cy.get('[name="login"]').type("unused1@yopmail.com");
+    cy.get('[type="submit"]').click();
+
+    cy.get('[name="password"]').type("password123");
+    cy.get('[action="/users/sign-in"]  [type="submit"]')
+      .contains("S’identifier")
+      .click();
+
+    for (let i = 0; i < 4; i++) {
+      cy.get("[name=totpToken]").type("123456");
+      cy.get(
+        '[action="/users/mfa-sign-in-with-authenticator"] [type="submit"]',
+      ).click();
+      cy.contains("le code que vous avez utilisé est invalide.");
+    }
+
+    cy.get("[name=totpToken]").type("123456");
+    cy.get(
+      '[action="/users/mfa-sign-in-with-authenticator"] [type="submit"]',
+    ).click();
+    cy.contains("Too Many Requests");
+  });
+});
