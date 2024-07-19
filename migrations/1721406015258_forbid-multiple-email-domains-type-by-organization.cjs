b1e1d6c54057a22941c8857a4961ab1acac0bae9
Raphaël Dubigny <rdubigny@gmail.com>
2024-07-19 18:26:46 +0200

    feat: add db constraint on domain type unicity

---
+++ migrations/1721406015258_forbid-multiple-email-domains-type-by-organization.cjs
@@ -0,0 +1,16 @@
+exports.shorthands = undefined;
+
+exports.up = async (pgm) => {
+  await pgm.db.query(`
+    ALTER TABLE email_domains
+      ADD CONSTRAINT unique_organization_domain
+        UNIQUE (organization_id, domain);
+  `);
+};
+
+exports.down = async (pgm) => {
+  await pgm.db.query(`
+    ALTER TABLE email_domains
+      DROP CONSTRAINT unique_organization_domain;
+  `);
+};
