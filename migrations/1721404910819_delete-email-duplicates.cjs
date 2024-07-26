1920174f84581500232c98e979583675dc45e1bc
Raphaël Dubigny <rdubigny@gmail.com>
2024-07-26 11:45:12 +0200

    feat: make sure there is no duplicates before adding database constraint

--- migrations/1721404910819_delete-email-duplicates.cjs
+++ migrations/1721404910819_delete-email-duplicates.cjs
@@ -1,6 +1,20 @@
 exports.shorthands = undefined;
 
 exports.up = async (pgm) => {
+  // Remove duplicate records
+  await pgm.db.query(`
+    WITH cte AS (SELECT id,
+                        ROW_NUMBER()
+                        OVER (PARTITION BY organization_id, domain, verification_type ORDER BY updated_at DESC) AS rn
+                 FROM email_domains)
+    DELETE
+    FROM email_domains
+    WHERE id IN (SELECT id
+                 FROM cte
+                 WHERE rn > 1);
+  `);
+
+  // Remove unverified domains if a verified equivalent exists
   await pgm.db.query(`
     DELETE FROM email_domains ed
     WHERE ed.verification_type IS NULL
@@ -9,7 +23,7 @@ exports.up = async (pgm) => {
       FROM email_domains ed_verified
       WHERE ed_verified.organization_id = ed.organization_id
         AND ed_verified.domain = ed.domain
-        AND ed_verified.verification_type = 'verified'
+        AND ed_verified.verification_type IN ('verified', 'trackdechets_postal_mail', 'external')
     );
   `);
 };
