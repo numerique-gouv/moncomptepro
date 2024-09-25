5763d134f794285d2dedad8f4db3f28e29208ab0
Benoit Serrano <benoit.serrano10@gmail.com>
2024-09-25 18:11:10 +0200

    put crisp inside ready trigger

--- assets/js/crisp.js
+++ assets/js/crisp.js
@@ -1,9 +1,11 @@
-window.$crisp = [];
-window.CRISP_WEBSITE_ID = "d1d5816e-314a-45e4-9715-144347b1039a";
-(function () {
-    d = document;
-    s = d.createElement("script");
-    s.src = "https://client.crisp.chat/l.js";
-    s.async = 1;
-    d.getElementsByTagName("head")[0].appendChild(s);
-})();
+window.CRISP_READY_TRIGGER = function() {
+  window.$crisp = [];
+  window.CRISP_WEBSITE_ID = "d1d5816e-314a-45e4-9715-144347b1039a";
+  (function () {
+      d = document;
+      s = d.createElement("script");
+      s.src = "https://client.crisp.chat/l.js";
+      s.async = 1;
+      d.getElementsByTagName("head")[0].appendChild(s);
+  })();
+};
