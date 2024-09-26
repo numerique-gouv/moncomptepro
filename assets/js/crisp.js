6af42d0a3a3ecca00772237c1c5c10d895499771
Benoit Serrano <benoit.serrano10@gmail.com>
2024-09-26 16:14:43 +0200

    replace crisp in file

--- assets/js/crisp.js
+++ assets/js/crisp.js
@@ -1,4 +1,4 @@
-window.CRISP_READY_TRIGGER = function() {
+// window.CRISP_READY_TRIGGER = function() {
   window.$crisp = [];
   window.CRISP_WEBSITE_ID = "d1d5816e-314a-45e4-9715-144347b1039a";
   (function () {
@@ -8,4 +8,4 @@ window.CRISP_READY_TRIGGER = function() {
       s.async = 1;
       d.getElementsByTagName("head")[0].appendChild(s);
   })();
-};
+// };
