85016b6f29e18a394c2a427336742c0fbf278aed
Douglas DUTEIL <douglasduteil@gmail.com>
2024-10-02 21:27:11 +0200

    feat: help adding crisp widget

--- assets/js/crisp.js
+++ assets/js/crisp.js
@@ -1,13 +1,13 @@
-window.$crisp = [];
+window.$crisp = [
+  [
+    "set",
+    "session:segments",
+    [["chat", "website", "identite", window.location.host]],
+  ],
+];
 window.CRISP_WEBSITE_ID = "d1d5816e-314a-45e4-9715-144347b1039a";
-window.CRISP_READY_TRIGGER = function() {
-  window.$crisp = [];
-  window.CRISP_WEBSITE_ID = "d1d5816e-314a-45e4-9715-144347b1039a";
-  (function () {
-      d = document;
-      s = d.createElement("script");
-      s.src = "https://client.crisp.chat/l.js";
-      s.async = 1;
-      d.getElementsByTagName("head")[0].appendChild(s);
-  })();
-};
+
+const script = document.createElement("script");
+script.src = "https://client.crisp.chat/l.js";
+script.async = 1;
+document.getElementsByTagName("head")[0].appendChild(script);
