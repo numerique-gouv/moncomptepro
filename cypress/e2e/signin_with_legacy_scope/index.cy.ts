07f5cb40994f2783d721d5d1b0b6772f4c1b49c8
Raphaël Dubigny <raphael.dubigny@beta.gouv.fr>
2024-10-21 18:05:25 +0200

    refactor: removed dedicated clients as they are now configurable

--- cypress/e2e/signin_with_legacy_scope/index.cy.ts
+++ cypress/e2e/signin_with_legacy_scope/index.cy.ts
@@ -1,13 +1,16 @@
 //
 
-describe("sign-in from legacy client", () => {
+describe("sign-in with legacy scope", () => {
   it("should sign-in", function () {
-    cy.visit("http://localhost:4002");
-    cy.get("button.proconnect-button").click();
+    cy.visit("http://localhost:4000");
+    cy.setCustomParams({
+      scope: "openid email profile phone organizations",
+    });
+    cy.get("button#custom-connection").click({ force: true });
 
     cy.login("unused1@yopmail.com");
 
-    cy.contains("moncomptepro-legacy-client");
+    cy.contains("moncomptepro-standard-client");
     cy.contains("unused1@yopmail.com");
     cy.contains("Commune de lamalou-les-bains");
 
