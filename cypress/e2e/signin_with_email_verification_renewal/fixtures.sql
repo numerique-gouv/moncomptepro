137c430f4a874e3cafef751559e1481fb5f71403
Raphaël Dubigny <raphael.dubigny@beta.gouv.fr>
2024-10-29 13:09:20 +0100

    tests: add tests on email verification

--- cypress/e2e/signin_with_email_verification_renewal/fixtures.sql
+++ cypress/e2e/signin_with_email_verification_renewal/fixtures.sql
@@ -2,7 +2,7 @@ INSERT INTO users
   (id, email, email_verified, email_verified_at, encrypted_password, created_at, updated_at, given_name, family_name,
    phone_number, job)
 VALUES
-  (1, 'bad1b70d-e5cb-436c-9ff3-f83d4af5d198@mailslurp.com', true, CURRENT_TIMESTAMP - INTERVAL '3 MONTHS 1 HOUR', '$2a$10$kzY3LINL6..50Fy9shWCcuNlRfYq0ft5lS.KCcJ5PzrhlWfKK4NIO', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'Jean', 'Jean1', '0123456789', 'Sbire');
+  (1, 'bad1b70d-e5cb-436c-9ff3-f83d4af5d198@mailslurp.com', true, CURRENT_TIMESTAMP - INTERVAL '3 MONTHS 1 HOUR', '$2a$10$kzY3LINL6..50Fy9shWCcuNlRfYq0ft5lS.KCcJ5PzrhlWfKK4NIO', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'Jean', 'Jean', '0123456789', 'Sbire');
 
 
 INSERT INTO organizations
