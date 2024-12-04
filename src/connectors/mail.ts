e915dfb237691ea3db1da6dc84508a6e61d4e981
Benoit Serrano <benoit.serrano10@gmail.com>
2024-12-04 15:04:26 +0100

    add subject prefix for mails in sandbox and local

--- src/connectors/mail.ts
+++ src/connectors/mail.ts
@@ -2,7 +2,7 @@
 
 import { convert } from "html-to-text";
 import { createTransport, type SendMailOptions } from "nodemailer";
-import { SMTP_URL } from "../config/env";
+import { DEPLOY_ENV, SMTP_URL } from "../config/env";
 
 //
 
@@ -17,12 +17,32 @@ interface SendMailBrevoOptions extends Omit<SendMailOptions, "from"> {
 }
 export function sendMail(options: SendMailBrevoOptions) {
   const tag = options.tag ? [{ key: "X-Mailin-Tag", value: options.tag }] : [];
+  const subject = computeMailSubject(options.subject);
 
   return transporter.sendMail({
     text:
       typeof options.html === "string" ? convert(options.html) : options.text,
     headers: [...tag],
     ...options,
+    subject,
     from: "nepasrepondre@email.moncomptepro.beta.gouv.fr",
   });
+}
+
+function computeMailSubject(
+  initialSubject: SendMailBrevoOptions["subject"],
+): SendMailBrevoOptions["subject"] {
+  if (initialSubject === undefined) {
+    return undefined;
+  }
+
+  switch (DEPLOY_ENV) {
+    case "localhost":
+      return `Local - ${initialSubject}`;
+    case "sandbox":
+      return `Test - ${initialSubject}`;
+    case "preview":
+      return `Test - ${initialSubject}`;
+  }
+  return initialSubject;
 }
