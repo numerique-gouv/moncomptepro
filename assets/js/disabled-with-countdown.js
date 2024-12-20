905dfaa16c81271d052a93b16ac057e8aed14a5f
rebeccadumazert <rebeccadumazert@hotmail.fr>
2024-12-20 15:30:37 +0100

    refacto(script): fix bugs on disabled with countdown script

--- assets/js/disabled-with-countdown.js
+++ assets/js/disabled-with-countdown.js
@@ -5,44 +5,26 @@ document.addEventListener(
 
     elements.forEach((element) => {
       const rawEndDate = element.getAttribute("data-countdown-end-date");
-
       try {
         const endDateInSeconds = new Date(rawEndDate).getTime() / 1000;
         const nowInSeconds = new Date().getTime() / 1000;
         let secondsToEndDate = Math.round(endDateInSeconds - nowInSeconds);
-        let intervalId;
+        element.disabled = true;
+        intervalId = setInterval(function () {
+          secondsToEndDate--;
 
-        if (isNaN(endDateInSeconds)) {
-          console.error("Invalid date provided in data-countdown-end-date.");
-          return;
-        }
-
-        if (secondsToEndDate > 0) {
-          element.disabled = true;
-
-          function updateButtonText() {
+          if (secondsToEndDate > 0) {
             const minutes = Math.floor(secondsToEndDate / 60);
             const seconds = String(secondsToEndDate % 60).padStart(2, "0");
             element.textContent = `Recevoir un nouvel email (disponible dans ${minutes}:${seconds})`;
           }
 
-          updateButtonText();
-
-          intervalId = setInterval(function () {
-            secondsToEndDate--;
-
-            if (secondsToEndDate > 0) {
-              updateButtonText();
-            } else {
-              clearInterval(intervalId);
-              element.disabled = false;
-              element.textContent = "Recevoir un nouvel email";
-            }
-          }, 1000);
-        } else {
-          element.disabled = false;
-          element.textContent = "Recevoir un nouvel email";
-        }
+          if (secondsToEndDate <= 0) {
+            element.textContent = `Recevoir un nouvel email`;
+            element.disabled = false;
+            return;
+          }
+        }, 1000);
       } catch (error) {
         // silently fails
       }
